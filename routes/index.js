b4a3087880252a477e84615640d7f3837eb9551a
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-05-16 11:39:26 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
+
+router.post("/upload", async (req, res) => {
+  const resultMove = await req.files.photo.mv(`./tmp/${req.files.photo.name}`);
+  const resultCloudinary = await cloudinary.uploader.upload(
+    `./tmp/${req.files.photo.name}`
+  );
+  fs.unlinkSync(`./tmp/${req.files.photo.name}`);
+
+  res.json({ result: true, url: resultCloudinary.secure_url });
+});
+
+module.exports = router;
